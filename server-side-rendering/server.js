f32785813eeb17552e34f9a484d1f0fb0e8eecc8
galbeiroc <alcresgu_1989@hotmail.com>
2021-12-27 11:58:52 -0500

    render an SSR react app

--- server-side-rendering/server.js
+++ server-side-rendering/server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
+import Home from './src/pages/Home';
 
 const app = express();
+app.use(express.static('./build', { index: false }));
 
 app.get('/*', (req, res) => {
   const reactApp = renderToString(
-    <h1>Renders from Server Side!</h1>
+    <Home />
   );
 
   return res.send(`
