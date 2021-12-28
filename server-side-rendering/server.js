f19a52eb6940142a603cd3568add68933a629e88
galbeiroc <alcresgu_1989@hotmail.com>
2021-12-28 11:29:03 -0500

    load data with SSR

--- server-side-rendering/server.js
+++ server-side-rendering/server.js
@@ -7,8 +7,22 @@ import fs from 'fs';
 import path from 'path';
 import App from './src/App';
 
+global.window = {};
+
 const app = express();
 app.use(express.static('./build', { index: false }));
+
+const articles = [
+  { title: "Article 1", author: 'Bob', description: "Setting the display property of an element only changes how the element is displayed" },
+  { title: "Article 2", author: 'Betty', description: "The following example displays <span> elements as block elements:" },
+  { title: "Article 3", author: 'Frank', description: "Changing an inline element to a block element, or vice versa, can be useful for making the page look a specific way" },
+  { title: "Article 4", author: 'Shaun', description: "Is commonly used with JavaScript to hide and show elements without deleting and recreating them" },
+]
+
+app.get('/api/articles', (req, res) => {
+  const loadedArticles = articles;
+  res.json(loadedArticles);
+})
 
 app.get('/*', (req, res) => {
   const sheet = new ServerStyleSheet();
@@ -25,8 +39,11 @@ app.get('/*', (req, res) => {
   fs.readFile(templateFile, 'utf-8', (err, data) => {
     if (err) return res.status(500).send(err);
     
+    const loadedArticles = articles;
+
+
     return res.send(
-      data.replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
+      data.replace('<div id="root"></div>', `<script>window.preloadedArticles = ${JSON.stringify(loadedArticles)};</script><div id="root">${reactApp}</div>`)
       .replace("{{ styles }}", sheet.getStyleTags())
     );
   });
--- server-side-rendering/src/pages/Article.js
+++ server-side-rendering/src/pages/Article.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Article = () => {
+  const [articles, setArticles] = useState(window && window.preloadedArticles);
+
+  useEffect(() => {
+    if (window && !window.preloadedArticles) {
+      console.log('No preloaded');
+      fetch('/api/articles')
+        .then((res) => res.json())
+        .then((data) => setArticles(data))
+        .catch((err) => console.log(err));
+    }
+  }, []);
+  console.log(articles)
   return (
-    <div>
-      <h1>Article</h1>
-    </div>
+    <>
+      <h1>Articles</h1>
+      {articles && articles.map(article =>(
+        <div key={article.title}>
+          <h4>{article.title}</h4>
+          <p>{article.author}</p>
+          <p>{article.description}</p>
+        </div>
+      ))}
+    </>
   )
 }
 
